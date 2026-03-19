8c2ea6d383a7236314a4163de8ff00574ea6a34e
Akbar U <91365919+urlenbayev@users.noreply.github.com>
2026-03-19 01:41:50 +0100

    6.9

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -4,7 +4,11 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ anecdotes, filter }) =>
+    anecdotes.filter(anecdote =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    )
+  )
 
   const vote = id => {
     dispatch(voteAnecdote(id))
